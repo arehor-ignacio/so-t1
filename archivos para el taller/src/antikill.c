5673f1897b81dfb19be94224e778786dabe85770
agus <afrenkel@dc.uba.ar>
2020-09-10 01:18:01 -0300

    ej 3, falta conseguir que tipo senial tira el kill

--- archivos para el taller/src/antikill.c
+++ archivos para el taller/src/antikill.c
@@ -6,10 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/user.h>
 
 int main(int argc, char* argv[]) {
 	int status;
 	pid_t child;
+	struct user_regs_struct uregs;
 
 	if (argc <= 1) {
 		fprintf(stderr, "Uso: %s comando [argumentos ...]\n", argv[0]);
@@ -20,6 +22,10 @@ int main(int argc, char* argv[]) {
 	child = fork();
 	if (child == -1) { perror("ERROR fork"); return 1; }
 	if (child == 0) {
+		if (ptrace(PTRACE_TRACEME, 0 , NULL , NULL)){
+			perror("ERROR child ptrace(PTRACE_TRACEME,...)");
+			exit(1);
+		}
 		/* S'olo se ejecuta en el Hijo */
 		execvp(argv[1], argv+1);
 		/* Si vuelve de exec() hubo un error */
@@ -27,9 +33,22 @@ int main(int argc, char* argv[]) {
 	} else {
 		/* S'olo se ejecuta en el Padre */
 		while(1) {
-			if (wait(&status) < 0) { perror("waitpid"); break; }
-			if (WIFEXITED(status)) break; /* Proceso terminado */
+			if (wait(&status) < 0) {perror("waitpid"); break; }
+			if (WIFEXITED(status)) break; /* Proceso terminado */ 
+			int sysno = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
+
+			if(sysno == 62){
+				int a = ptrace(PTRACE_GETREGS, child, NULL, &uregs);
+				printf("%lli\n", uregs.rax);
+				//int b = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RCX, NULL);
+			}
+			
+			
+			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+
 		}
+
+		
 	}
 	return 0;
 }
